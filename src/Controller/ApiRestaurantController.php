92aefa05ad40bab63e6c3a408e902c51086a9f62
Fzammit <frederic.zammit@hotmail.fr>
2020-03-24 22:32:02 +0100

    post :godmode:

--- src/Controller/ApiRestaurantController.php
+++ src/Controller/ApiRestaurantController.php
@@ -2,10 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\City;
 use App\Entity\Restaurant;
+use App\Entity\User;
+use Doctrine\Common\Annotations\AnnotationReader;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactory;
+use Symfony\Component\Serializer\Mapping\Loader\AnnotationLoader;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
 
@@ -15,21 +22,68 @@ class ApiRestaurantController extends AbstractController
 
     public function __construct()
     {
+        $classMetadataFactory = new ClassMetadataFactory(new AnnotationLoader(new AnnotationReader()));
         $encoders = [new JsonEncoder()];
-        $normalizers = [new ObjectNormalizer()];
+        $normalizers = [new ObjectNormalizer($classMetadataFactory)];
 
         $this->serializer = new Serializer($normalizers, $encoders);
     }
 
     /**
-     * @Route("/api/restaurant", name="api_restaurant")
+     * @Route("/api/restaurant", name="api_restaurant", methods={"GET"})
      */
     public function index()
     {
         $restaurants = $this->getDoctrine()->getRepository(Restaurant::class)->findAll();
 
-        $jsonContent = $this->serializer->serialize($restaurants, 'json');
+        $data = $this->serializer->normalize($restaurants, null, ['groups' => 'al_restaurants']);
 
-        echo $jsonContent;
+        $jsonContent = $this->serializer->serialize($data, 'json');
+
+        $response = new Response($jsonContent);
+        $response->headers->set('content-Type', 'application/json');
+
+        return $response;
+    }
+
+    /**
+     * @Route("/api/restaurants", name="api_restaurant_create", methods={"POST"})
+     */
+    public function create( Request $request)
+    {
+
+        /**
+         * On lit les données de la requête :
+         */
+        dump($request->request);
+
+        /**
+         * Pour lire un champ de la requête, parmi les champs donnés dans Postman (ici, le
+         * champ 'name' par exemple)
+         */
+        dd($request->request->get('name'));
+
+        /**
+         * On créée un restaurant en prenant les données de la requête
+         */
+        $restaurant = new Restaurant;
+        $restaurant->setName('Nouveau resto');
+        $restaurant->setDescription('Une description !');
+
+        /**
+         * On récupère les users 1 et city 1 (car l'objet Restaurant s'attend à des objets)
+         */
+        $user = $this->getDoctrine()->getRepository(User::class)->find(1);
+        $city = $this->getDoctrine()->getRepository(City::class)->find(1);
+
+        $restaurant->setUser($user);
+        $restaurant->setCity($city);
+
+        /**
+         * On enregistre en base de données
+         */
+        $manager = $this->getDoctrine()->getManager();
+        $manager->persist($restaurant);
+        $manager->flush();
     }
 }
--- src/Entity/Restaurant.php
+++ src/Entity/Restaurant.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\RestaurantRepository")
@@ -15,44 +16,44 @@ class Restaurant
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
-     * 
+     * @Groups("all_restaurants")
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
-     * 
+     * @Groups("all_restaurants")
      */
     private $name;
 
     /**
      * @ORM\Column(type="text", nullable=true)
-     * 
+     * @Groups("all_restaurants")
      */
     private $description;
 
     /**
      * @ORM\Column(type="datetime")
-     * 
+     * @Groups("all_restaurants")
      */
     private $createdAt;
 
     /**
      * @ORM\ManyToOne(targetEntity="App\Entity\City", inversedBy="restaurants")
      * @ORM\JoinColumn(nullable=false)
-     * 
+     * @Groups("all_restaurants")
      */
     private $city;
 
     /**
      * @ORM\OneToMany(targetEntity="App\Entity\RestaurantPicture", mappedBy="restaurant", orphanRemoval=true)
-     * 
+     * @Groups("all_restaurants")
      */
     private $restaurantPictures;
 
     /**
      * @ORM\OneToMany(targetEntity="App\Entity\Review", mappedBy="restaurant", orphanRemoval=true)
-     * 
+     * @Groups("all_restaurants")
      */
     private $reviews;
 
